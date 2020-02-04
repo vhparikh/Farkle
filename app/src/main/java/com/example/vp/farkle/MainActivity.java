634b5bc11ad2aaabec490bd1d402e5eaca0e29c9
vhparikh <42686619+vhparikh@users.noreply.github.com>
2020-02-04 09:07:50 -0800

    activity beginnings

--- app/src/main/java/com/example/vp/farkle/MainActivity.java
+++ app/src/main/java/com/example/vp/farkle/MainActivity.java
@@ -3,12 +3,55 @@ package com.example.vp.farkle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextClock;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ImageButton[] buttons = new ImageButton[6];
+    int[] buttonState = new int[6];
+    final int HOT_DIE = 0;
+    final int SCORE_DIE = 1;
+    final int LOCKED_DIE = 2;
+    Button roll;
+    Button score;
+    Button stop;
+    TextView currentScoreTV;
+    TextView totalScoreTV;
+    TextView currentRoundTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttons[0] = (ImageButton)this.findViewById(R.id.imageButton1);
+        buttons[1] = (ImageButton)this.findViewById(R.id.imageButton2);
+        buttons[2] = (ImageButton)this.findViewById(R.id.imageButton3);
+        buttons[3] = (ImageButton)this.findViewById(R.id.imageButton4);
+        buttons[4] = (ImageButton)this.findViewById(R.id.imageButton5);
+        buttons[5] = (ImageButton)this.findViewById(R.id.imageButton6);
+        for(int a = 0; a < buttons.length; a++){
+            buttons[a].setOnClickListener(this);
+            buttons[a].setEnabled(false);
+        }
+        roll = (Button)this.findViewById(R.id.button1);
+        roll.setOnClickListener(this);
+        score = (Button)this.findViewById(R.id.button2);
+        score.setOnClickListener(this);
+        score.setEnabled(false);
+        stop = (Button)this.findViewById(R.id.button3);
+        stop.setOnClickListener(this);
+        stop.setEnabled(false);
+        currentScoreTV = (TextView)this.findViewById(R.id.textView1);
+        totalScoreTV = (TextView)this.findViewById(R.id.textView2);
+        currentRoundTV = (TextView)this.findViewById(R.id.textView3);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
